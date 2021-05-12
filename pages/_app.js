c85d76c4c6242c98c4bc007ef20bbb0c3d99c454
Danny Aziz <daanyaal997@gmail.com>
2021-05-12 17:37:59 +0100

    feat: Mobile Header with Animations

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
+
 import { Reset } from "styled-reset";
 import { GlobalStyle, lightTheme, darkTheme } from "@/utils/theme";
 import { ThemeProvider } from "styled-components";
 
-const App = ({ Component, pageProps }) => (
-  <ThemeProvider theme={lightTheme}>
-    <Reset />
-    <GlobalStyle />
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
+import Header from "@/components/header/index.js";
+
+const App = ({ Component, pageProps }) => {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <Reset />
+      <GlobalStyle />
+      <Header theme={theme} setTheme={setTheme} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
 
 export default App;
